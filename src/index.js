a3ae61eaa54f33fb1086b9de1e78c8accfc5ede2
Noel Gitari <gitarinoel@gmail.com>
2023-03-26 11:30:19 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form =document.querySelector("form")
+  form.addEventListener("submit",(e)=>{
+   e.preventDefault()
+    buildTodo(e.target.new_task_description.value)
+    form.reset()
+ })
 });
+function buildTodo(todo){
+  let p=document.createElement('p')
+  let btn=document.createElement('button')
+  btn.addEventListener('click',handleDelete)
+  btn.textContent= 'X'
+  p.textContent=`${todo}`
+
+  p.appendChild(btn)
+ // console.log(p)
+  document.querySelector('#list').appendChild(p)
+}
+function handleDelete(e){
+   e.target.parentNode.remove()
+
+}
